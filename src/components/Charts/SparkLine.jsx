c44b6d19911680a153f9689d3df56e9cdc2d57d2
Tanish Garg <tanishgarg1939@gmail.com>
2024-07-25 06:54:43 +0530

    Add front page and dashboard structure

---
+++ src/components/Charts/SparkLine.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import {
+  SparklineComponent,
+  Inject,
+  SparklineTooltip,
+} from "@syncfusion/ej2-react-charts";
+
+const SparkLine = ({ id, type, height, width, color, currentColor, data }) => {
+  return (
+    <SparklineComponent
+      id={id}
+      height={height}
+      width={width}
+      lineWidth={1}
+      valueType="Numeric"
+      fill={color}
+      border={{ color: currentColor, width: 2 }}
+      dataSource={data}
+      type={type}
+      xName="x"
+      yName="yval"
+      tooltipSettings={{
+        visible: true,
+        format: "${x} : data ${yval}",
+        trackLineSettings: {
+          visible: true,
+        },
+      }}
+    >
+      <Inject services={[SparklineTooltip]} />
+    </SparklineComponent>
+  );
+};
+
+export default SparkLine;
